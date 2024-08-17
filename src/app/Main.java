b7f79a1b8bce8087e27f132bfc7c98edcf093480
EdAlekseiev <e.alekseiev@gmail.com>
2024-08-17 12:31:42 +0300

    Add converting from miles to kms
    - Add coefficient of converting from miles to kms CONV_K to Main class
    - Add method convMilesToKms to Main class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,20 @@
 package app;
 
 public class Main {
+
+    public static final double CONV_K = 0.62137;
+
     public static void main(String[] args) {
         System.out.println("App for distance converting.");
-        System.out.println("Version 1.0.");
+        System.out.println("Version 1.1.");
+
+        double miles = 120;
+        double kms = convMilesToKms(miles);
+
+        System.out.println(miles + " miles are " + kms + " kms.");
+    }
+
+    private static double convMilesToKms(double miles) {
+        return miles / CONV_K;
     }
 }
